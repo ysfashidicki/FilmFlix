a06c663b676c0a826d0d5814fd13b95980b7d8c9
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-06-20 22:19:32 +0700

    fix: logo favorites

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,13 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 
-import { GiHamburgerMenu } from "react-icons/gi";
 import { TbMovie } from "react-icons/tb";
-import { GrClose } from "react-icons/gr";
+import { MdFavorite } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export const Navbar: FC = () => {
-  const [open, setOpen] = useState(false);
   return (
     <div className="shadow-md w-full top-0 left-0 bg-transparent">
-      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
+      <div className="flex items-center justify-between py-4 md:px-10 px-7">
         <Link
           to={"/"}
           className="font-bold text-2xl cursor-pointer flex items-center"
@@ -19,19 +17,11 @@ export const Navbar: FC = () => {
           </span>
           FilmFlix
         </Link>
-        <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-        >
-          {open ? <GrClose /> : <GiHamburgerMenu />}
-        </div>
-        <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 " : "top-[-490px]"
-          }`}
-        >
-          <li className="md:ml-8 text-xl md:my-0 my-7 duration-500 cursor-pointer hover:scale-125 transition-all ease-in-out">
-            <Link to={"/favorites"}>Favorites Movie</Link>
+        <ul>
+          <li className="md:ml-8 text-xl duration-500 cursor-pointer hover:scale-125 transition-all ease-in-out">
+            <Link to={"/favorites"}>
+              <MdFavorite size={45} />
+            </Link>
           </li>
         </ul>
       </div>
