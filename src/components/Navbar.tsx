e26e8ab8d2a3140b72ea4a33f4851c2355b5482c
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-06-21 12:59:39 +0700

    feat: dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,29 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 
 import { TbMovie } from "react-icons/tb";
 import { MdFavorite } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export const Navbar: FC = () => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.setAttribute("data-theme", "forest");
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "garden");
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
   return (
     <div className="shadow-md w-full top-0 left-0 bg-transparent">
       <div className="flex items-center justify-between py-4 md:px-10 px-7">
@@ -17,13 +36,19 @@ export const Navbar: FC = () => {
           </span>
           FilmFlix
         </Link>
-        <ul>
-          <li className="md:ml-8 text-xl duration-500 cursor-pointer hover:scale-125 transition-all ease-in-out">
+        <div className="flex items-center space-x-7">
+          <div className="md:ml-8 text-xl duration-500 cursor-pointer hover:scale-125 transition-all ease-in-out">
             <Link to={"/favorites"}>
               <MdFavorite size={45} />
             </Link>
-          </li>
-        </ul>
+          </div>
+          <input
+            type="checkbox"
+            className="toggle toggle-lg"
+            onChange={toggleDarkMode}
+            checked={darkMode}
+          />
+        </div>
       </div>
     </div>
   );
